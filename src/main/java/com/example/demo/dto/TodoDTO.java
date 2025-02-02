41dd658c27f74f163a317299590dc74c71355185
leesooim <soojeng22@outlook.com>
2025-02-02 14:52:03 +0900

    TodoApp project BackEnd SetUp

---
+++ src/main/java/com/example/demo/dto/TodoDTO.java
@@ -0,0 +1,27 @@
+package com.example.demo.dto;
+
+import java.util.UUID;
+
+import com.example.demo.model.TodoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoDTO {
+
+	private UUID id;
+	private String title;
+	private boolean done;
+	
+	public TodoDTO(final TodoEntity entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.done = entity.isDone();
+	}
+
+}
